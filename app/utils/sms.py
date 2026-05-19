b55b37a7abbb5df78e574404acd1c8c7d18e8f26
ebayliss-dev <edward.bayliss@icloud.com>
2026-05-19 10:31:54 +0100

    test

--- app/utils/sms.py
+++ app/utils/sms.py
@@ -5,14 +5,14 @@ import vonage
 def send_magic_link_sms(phone: str, link: str):
 
 # if you want to manage your secret, please do so by visiting your API Settings page in your dashboard
-    client = vonage.Client(key="fb59843d", secret=os.getenv("VONAGE_API_SECRET"))
+    client = vonage.Client(key="fb59843d", secret="VUjJWeQiA0fVbZq)j)NzZ2P")
     sms = vonage.Sms(client)
 
     message = link
 
     response = sms.send_message(
         {
-            "from": "BurtonAleTrail",
+            "from": "BAT",
             "to": phone,
             "text": message,
         }
@@ -20,5 +20,3 @@ def send_magic_link_sms(phone: str, link: str):
 
     if response["messages"][0]["status"] != "0":
         raise Exception(response["messages"][0].get("error-text"))
-    
-
